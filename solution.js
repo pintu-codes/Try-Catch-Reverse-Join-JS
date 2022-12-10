5b26d8e9062035d54929ec76a5a07e9cbdf2700e
Pintu Sarkar <pintucodes@gmail.com>
2022-12-10 19:16:01 +0530

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,10 @@
+function reverseString(s) {
+   try{
+       s = s.split('').reverse().join('');
+   } catch(e){
+       console.log("s.split is not a function");
+   }
+   console.log(s);
+}
+
+console.log(reverseString("1234"));
